90105a3107ce28b80d9010a2630e7ed293a6741e
Pete Voulgaris <petevoulgaris@gmail.com>
2025-02-19 20:18:20 -0500

    clean-up files

--- HelloWorld/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_appHelloWorld_FILE C:/_WorkLog/_Code/Qt/HelloWorld/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/appHelloWorld.exe)
-set(__QT_DEPLOY_TARGET_appHelloWorld_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_appHelloWorld_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6QmlMeta.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
