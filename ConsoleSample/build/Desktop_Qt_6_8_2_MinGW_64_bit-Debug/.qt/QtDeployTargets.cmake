8fb7398644033be096ad24b5c2c908e3d4b8cfd4
Pete Voulgaris <petevoulgaris@gmail.com>
2025-02-19 20:12:13 -0500

    updated Qt sample

---
+++ ConsoleSample/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ConsoleSample_FILE C:/_WorkLog/_Code/Qt/ConsoleSample/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ConsoleSample.exe)
+set(__QT_DEPLOY_TARGET_ConsoleSample_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ConsoleSample_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
---
+++ HelloWorld/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appHelloWorld_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appHelloWorld")
+set(working_dir "C:/_WorkLog/_Code/Qt/HelloWorld")
+set(src_and_dest_list
+
+)
+set(timestamp_file "C:/_WorkLog/_Code/Qt/HelloWorld/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appHelloWorld_res.txt")
