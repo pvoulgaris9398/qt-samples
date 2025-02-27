f142722191fa77c95d6aa7dcc44831f11662d234
Pete Voulgaris <petevoulgaris@gmail.com>
2025-02-26 19:36:25 -0500

    updated config and tried to build qt-mqtt sample

---
+++ qt-mqtt/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef _MAINWINDOW_H
+#define _MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMqttClient>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow {
+Q_OBJECT
+public:
+exlicit MainWindow(QWidget* parent = nullptr);
+~MainWindow();
+
+public slots:
+    void setClientPort(int p);
+
+private slots:
+    void on_buttonConnect_clicked();
+    void on_buttonQuit_clicked();
+    void updateLogStateChange();
+
+    void brokerDisconnected();
+
+    void on_buttonPublish_clicked();
+
+    void on_buttonSubscribed_clicked();
+
+private:
+
+    Ui::MainWindow *ui;
+    QMqttClient *m_client;
+}
+
+#endif // _MAINWINDOW_H
